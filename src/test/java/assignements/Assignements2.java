66a25d6dddc6efbdcaf937c1fb2c6f2af8731747
Rambabu1064 <ramisetti.rambabu089@gmail.com>
2025-07-04 13:43:06 +0530

    Add files via upload

---
+++ src/test/java/assignements/Assignements2.java
@@ -0,0 +1,66 @@
+package assignements;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+                                 //Info : Assignement 2 - Amazon Drop down X paths//
+public class Assignements2 {
+	WebDriver driver = new ChromeDriver();
+
+	@BeforeTest()
+	public void OpenURL() {
+	driver.get("https://www.amazon.in/");
+	driver.manage().window().maximize();
+	driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+	
+	
+
+}
+	@Test
+	public void Locators() {
+		WebElement AccountList = driver.findElement(By.xpath("//*[@id=\"nav-link-accountList\"]/button"));
+		AccountList.click();
+		
+		//Create WishList
+		WebElement WishList = driver.findElement(By.className("nav-text"));
+		WishList.click();
+		driver.navigate().back();
+		
+		//WishFromAnyWebsite
+		WebElement AccountList1 = driver.findElement(By.xpath("//*[@id=\"nav-link-accountList\"]/button"));
+		AccountList1.click();
+		WebElement WishFromAnyWebsite = driver.findElement(By.xpath("//span[text()='Wish from Any Website']/ancestor::a[contains(@class,'nav-link')]"));
+		WishFromAnyWebsite.click();
+		
+		
+		//BabyWishList
+		WebElement AccountList2 = driver.findElement(By.xpath("//*[@id=\"nav-link-accountList\"]/button"));
+		AccountList2.click();
+		WebElement BabyWishList = driver.findElement(By.xpath("//span[text()='Baby Wishlist']/ancestor::a[contains(@class,'nav-link')]"));
+		BabyWishList.click();
+		
+		//DiscoverYourStyle
+		WebElement AccountList3 = driver.findElement(By.xpath("//*[@id=\"nav-link-accountList\"]/button"));
+		AccountList3.click();
+		WebElement DiscoverYourStyle = driver.findElement(By.xpath("//span[text()='Discover Your Style']/ancestor::a[contains(@class,'nav-link')]"));
+		DiscoverYourStyle.click();
+		
+		//ExploreShowroom
+		WebElement AccountList4 = driver.findElement(By.xpath("//*[@id=\"nav-link-accountList\"]/button"));
+		AccountList4.click();
+		WebElement ExploreShowroom = driver.findElement(By.xpath("//span[text()='Explore Showroom']/ancestor::a[contains(@class,'nav-link')]"));
+		ExploreShowroom.click();
+	}
+	@AfterTest()
+	
+		public void CloseURL() {
+			
+			driver.close();
+		}
+	}
---
+++ src/test/java/assignements/Assignements5.java
@@ -0,0 +1,65 @@
+package assignements;
+
+import java.time.Duration;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+import org.openqa.selenium.support.ui.Select;
+               
+                       //Info : Assignement 5 - RahulShetty Drop down x paths For Selecting countries option//
+public class Assignements5 {
+	ChromeDriver driver = new ChromeDriver();
+	
+
+		@BeforeTest()
+		public void OpenUrl()
+		{
+			driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+			driver.manage().window().maximize();
+			driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+		 	}
+		@Test() 
+		public void dropDown()
+		{
+			WebElement dropdownElement =driver.findElement(By.id("dropdown-class-example"));
+			Select dropdown = new Select(dropdownElement);
+			dropdown.selectByVisibleText("Option2");
+			dropdown.getFirstSelectedOption();
+			WebElement selectedOption = dropdown.getFirstSelectedOption();
+			System.out.println("Selected option " + selectedOption.getText());
+			
+		}
+		@Test()
+		public void SuggestionClass () 
+		{
+			WebElement selectCountries = driver.findElement(By.id("autocomplete"));
+			selectCountries.sendKeys("Russ");
+			
+			List<WebElement> dropdownOptions = driver.findElements(By.xpath("//li[contains(@class, 'ui-menu-item')]/div"));
+			
+			for(WebElement options:dropdownOptions) {
+			if(options.getText().equalsIgnoreCase("Russian federation")) {
+			options.click();
+				}
+		    }
+				
+			
+		    String selectedValue = selectCountries.getAttribute("value");
+		    System.out.println(selectedValue);
+		 
+			}
+//			@AfterTest
+//			public void Close() {
+//				driver.close();
+//			}
+//		
+			
+		
+		}
+	
+
